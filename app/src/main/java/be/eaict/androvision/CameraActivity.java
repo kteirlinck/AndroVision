41eeea0e1b249b06d66857ba4c485ebd9689fe8a
Unknown <keno.vandeuren@student.ap.be>
2018-12-11 09:29:50 +0100

    foto trekken
    je kan nu een foto trekken en aan de gebruiker vragen voor bevestiging

--- app/src/main/java/be/eaict/androvision/CameraActivity.java
+++ app/src/main/java/be/eaict/androvision/CameraActivity.java
@@ -1,15 +1,40 @@
 package be.eaict.androvision;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class CameraActivity extends AppCompatActivity {
+
+    Button btnCamera;
+    ImageView cameraView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
+        btnCamera = (Button) findViewById(R.id.btnCamera);
+        cameraView = (ImageView) findViewById(R.id.CameraView);
 
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        cameraView.setImageBitmap(bitmap);
     }
 }
